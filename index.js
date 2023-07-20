629d12f49af75b6c73e319f8da6f8d987082fc7e
ByteBane <81875670+bytebane@users.noreply.github.com>
2023-07-20 22:57:05 +0530

    Theme Fixed
    Improved toggle theme function and changed its location from bottom to top right.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const checkbox = document.getElementById("checkbox")
-checkbox.addEventListener("change", () => myFunction())
-function myFunction() {
+checkbox.addEventListener("change", () => switchTheme())
+function switchTheme() {
 	const myBody = document.body;
 	const mySourceCode = document.getElementById('sourceCode');
+	const mySourceCodeKey = document.querySelectorAll('.codeObjKey');
+	const mySourceCodeVal = document.querySelectorAll('.codeObjVal');
 
 	myBody.classList.toggle("darkModeBody");
 	mySourceCode.classList.toggle("darkModeSourceCode");
+	// mySourceCodeKey.classList.toggle("darkModeCodeObjKey");
+	// mySourceCodeVal.classList.toggle("darkModeCodeObjVal");
+	mySourceCodeKey.forEach((element) => {
+		element.classList.toggle("darkModeCodeObjKey");
+	});
+	mySourceCodeVal.forEach((element) => {
+		element.classList.toggle("darkModeCodeObjVal");
+	});
 }
+
+
+window.addEventListener("scroll", function() {
+	const checkboxLabel = document.querySelector(".checkbox-label");
+	if (window.scrollY > 0) {
+		checkboxLabel.style.visibility = "hidden";
+	} else {
+		checkboxLabel.style.visibility = "visible";
+	}
+});
